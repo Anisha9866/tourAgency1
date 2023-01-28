7f2d52025589b11d7612018384dae2f10aa63d64
Abhijit Salunkhe <95414910+Anisha9886@users.noreply.github.com>
2023-01-28 23:35:23 +0530

    code changed

--- login/student/urls.py
+++ login/student/urls.py
@@ -23,6 +23,5 @@ urlpatterns = [
     path('',include('portal.urls')),
     path('register/',dash_views.register,name='register'),
     path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
-    path('profile/',dash_views.profile,name='profile'),
     path('logout/',auth_views.LogoutView.as_view(template_name='logout.html'),name='logout'),
 ]
